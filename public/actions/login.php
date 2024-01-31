195017142ebf943e4975da822ed61c1f14479e46
Romuald BRISACIER <romualdbrisacier@MacBook-Pro-de-coding.local>
2024-01-31 15:18:03 +0100

    ajout de la connexion utilisateur

--- public/actions/login.php
+++ public/actions/login.php
@@ -16,4 +16,19 @@ if (empty($_POST['pswrd'])) {
 if (isset($_POST['username']) && isset($_POST['pswrd'])) {
     $username = htmlspecialchars($_POST['username']);
     $pswrd = hash('sha256', $_POST['pswrd']);
+    $st1 = $pdo->prepare('SELECT * FROM users WHERE username = ? and pswrd = ?');
+    $st1->execute(array($username, $pswrd));
+
+    if ($st1->rowCount() > 0) {
+        $_SESSION['username'] = $username;
+        $_SESSION['pswrd'] = $pswrd;
+        $_SESSION['id'] = $st1->fetch()['id'];
+        $_SESSION['error_message'] = 'Bonjour ' . $_SESSION['username'] . ' et bienvenue.';
+        header('Location: /login.php');
+        die();
+    } else {
+        $_SESSION['error_message'] = 'Votre mot de passe ou pseudo est incorrect.';
+        header('Location: /login.php');
+        die();
+    }
 }
